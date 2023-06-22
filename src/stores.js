018ad8e674bdce6fbb33a6e4836822addfc9e134
Adhavan <asivaraj@iihs.ac.in>
2023-06-22 17:10:25 +0530

    formatting: minor formatting updates

--- src/stores.js
+++ src/stores.js
@@ -1,12 +1,12 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 // Nodes
 const retrieved = localStorage.getItem("storedFeatures");
-const parsed = JSON.parse(retrieved)
-export const selectedFeatures = writable(parsed === null ? [] : parsed)
+const parsed = JSON.parse(retrieved);
+export const selectedFeatures = writable(parsed === null ? [] : parsed);
 
-selectedFeatures.subscribe(value => {
-    localStorage.setItem("storedFeatures", JSON.stringify(value))
+selectedFeatures.subscribe((value) => {
+  localStorage.setItem("storedFeatures", JSON.stringify(value));
 });
 
 export const uploadedSources = writable([]);

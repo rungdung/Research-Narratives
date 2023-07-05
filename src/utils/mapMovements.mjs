f9776c0ebd70c47bd6568719e3b694f24cf7383b
Adhavan <asivaraj@iihs.ac.in>
2023-07-05 10:18:05 +0530

    Workaround: Generalise key name of maplibre feature

--- src/utils/mapMovements.mjs
+++ src/utils/mapMovements.mjs
@@ -1,32 +1,41 @@
 // convert to maplibre
-import maplibre from 'maplibre-gl';
-import * as Turf from '@turf/turf';
-
+import maplibre from "maplibre-gl";
+import * as Turf from "@turf/turf";
 
 export function zoomToFeature(e, map) {
-  let point
+  let point;
+  let geometry = e._geometry ? e._geometry : e.geometry;
   // highlightLayer.clearLayers();
-  if (e._geometry.type == 'Polygon') {
-    point = Turf.centerOfMass(Turf.geometry('Polygon', e._geometry.coordinates))
-      .geometry;
-  } else if (e._geometry.type == 'MultiPolygon') {
+  if (geometry.type == "Polygon") {
+    point = Turf.centerOfMass(
+      Turf.geometry("Polygon", geometry.coordinates)
+    ).geometry;
+  } else if (geometry.type == "MultiPolygon") {
     // get a array of polygons
-    point = Turf.centerOfMass(Turf.geometry('MultiPolygon', e._geometry.coordinates))
-      .geometry;
-  } else if (e._geometry.type == 'LineString') {
-    point = Turf.centerOfMass(Turf.geometry('LineString', e._geometry.coordinates))
-      .geometry;
-  } else if (e._geometry.type == 'MultiLineString') {
-    point = Turf.centerOfMass(Turf.geometry('MultiLineString', e._geometry.coordinates))
-      .geometry;
-  } else if (e._geometry.type == 'Point') {
-    point = Turf.geometry('Point', e._geometry.coordinates);
-    console.log(point)
+    point = Turf.centerOfMass(
+      Turf.geometry("MultiPolygon", geometry.coordinates)
+    ).geometry;
+  } else if (geometry.type == "LineString") {
+    point = Turf.centerOfMass(
+      Turf.geometry("LineString", geometry.coordinates)
+    ).geometry;
+  } else if (geometry.type == "MultiLineString") {
+    point = Turf.centerOfMass(
+      Turf.geometry("MultiLineString", geometry.coordinates)
+    ).geometry;
+  } else if (geometry.type == "Point") {
+    point = Turf.geometry("Point", geometry.coordinates);
+    console.log(point);
   }
   map.flyTo({
     center: point.coordinates,
     zoom: 10,
-    essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+    speed: 1,
+    curve: 2,
+    easing(t) {
+      return t;
+    },
+    // this animation is considered essential with respect to prefers-reduced-motion
   });
 }
 

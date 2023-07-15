4c6baff4d59cd674882149442736e2ab966f18a6
Adhavan <asivaraj@iihs.ac.in>
2023-07-15 15:17:08 +0530

    File reorgnaisation: Split main page into its own component away from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,12 @@
 <script>
-  import Map, { map } from "./components/Map.svelte";
-  import ResearchMap from "./components/ResearchMap.svelte";
-  import Filter from "./components/Filter.svelte";
-  import FileUploadModal from "./components/FileUploadModal.svelte";
-  import FileNavigator from "./components/FileNavigator.svelte";
   import { loadDataFromDB } from "./utils/loadFromDB.mjs";
   import { uploadedSources, markupNodes, narrativeNodes } from "./stores";
-  import SearchSemanticScholar from "./components/SearchSemanticScholar.svelte";
-  import { loadSources } from "./utils/loadFromDB.mjs";
-  import { onMount, tick } from "svelte";
-  import {
-    Drawer,
-    Button,
-    CloseButton,
-    Accordion,
-    AccordionItem,
-  } from "flowbite-svelte";
-  import { OpenBookSolid } from "flowbite-svelte-icons";
-  import { sineIn } from "svelte/easing";
+
+  import { onMount } from "svelte";
 
   import { Route, Router } from "svelte-routing";
   import Template from "./components/routes/Template.svelte";
-
-  let hiddenResearchMap = true;
-  let transitionParams = {
-    x: -320,
-    duration: 700,
-    easing: sineIn,
-  };
-
+  import Main from "./components/routes/Main.svelte";
   export let supabase;
 
   //set stores
@@ -55,164 +33,10 @@
   </Route>
 
   <Route path="/">
-    <main>
-      <div id="mapContainer">
-        <Map />
-
-        <div id="left-bar" class="p-4">
-          <div id="meta-info" class="">
-            <h1 class="font-bold text-4xl">Narrative Maker</h1>
-            <h3>
-              This is a prototype to explore linked stories of geospatial data
-            </h3>
-          </div>
-
-          <Accordion flush>
-            <aside id="data-ops">
-              <AccordionItem
-                class="bg-primary-50 !py-0"
-                paddingFlush="py-1"
-                padding="p-0"
-              >
-                <h3 slot="header">Filter</h3>
-                <section id="data-ops-inner">
-                  <div id="filter" class="m-3 px-3">
-                    <Filter />
-                  </div>
-                </section>
-              </AccordionItem>
-            </aside>
-
-            <aside id="sources">
-              <AccordionItem class="bg-primary-50" paddingFlush="py-1" open>
-                <h3 slot="header">Sources</h3>
-                <section id="sources-inner">
-                  <div id="parseFile" class="m-2 px-3">
-                    <FileNavigator />
-                  </div>
-
-                  <div id="searchSemanticScholar" class="m-2 px-3">
-                    <SearchSemanticScholar />
-                  </div>
-                </section>
-              </AccordionItem>
-            </aside>
-          </Accordion>
-        </div>
-      </div>
-
-      <span id="right-bar-trigger" class="text-center">
-        <Button
-          color="dark"
-          class="z-10 absolute bg-primary-50 text-black hover:text-white top-5 right-5 w-50"
-          on:click={() => (hiddenResearchMap = false)}
-        >
-          <OpenBookSolid color="light" svgClass="mr-3" />Open research map</Button
-        >
-      </span>
-      <Drawer
-        placement="right"
-        transitionType="fly"
-        {transitionParams}
-        bind:hidden={hiddenResearchMap}
-        id="right-bar"
-      >
-        <CloseButton
-          on:click={() => (hiddenResearchMap = true)}
-          id="closeButton"
-        />
-        <section id="markup" class="">
-          <h2 class="text-2xl">Research map</h2>
-          <p class="">
-            Imagine this to be a mindmap. Draw connections between documents as
-            you go along
-          </p>
-        </section>
-
-        <ResearchMap {supabase} />
-      </Drawer>
-      <FileUploadModal {supabase} />
-    </main>
+    <Main {supabase} />
   </Route>
 </Router>
 <svelte:window />
 <svelte:head>
   <title>Research narrative building</title>
 </svelte:head>
-
-<style global>
-  main {
-    max-height: 100vh;
-    overflow: hidden;
-  }
-  :global(#closeButton) {
-    position: absolute;
-    top: 2%;
-    right: 2%;
-    background-color: #475569;
-  }
-  #markup {
-    margin-bottom: 2em;
-    padding: 0em;
-    font-size: larger;
-  }
-  main {
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-  }
-  #mapContainer {
-    width: 100vw;
-    height: 100vh;
-  }
-  #left-bar {
-    position: absolute;
-    top: 2%;
-    left: 2%;
-    max-height: 100vh;
-    text-align: left;
-    width: 20%;
-    color: black;
-  }
-
-  #sources,
-  #data-ops,
-  #meta-info {
-    border-radius: 10px;
-    width: 100%;
-    margin-top: 1em;
-    color: black;
-    background-color: #faebd7;
-  }
-
-  #meta-info {
-    padding: 1em;
-  }
-  :global(.card) {
-    padding: 0.2em;
-  }
-  :global(.card-header) {
-    padding: 0em;
-    color: black !important;
-  }
-
-  #right-bar-trigger {
-    position: absolute;
-    top: 2%;
-    right: 2%;
-    z-index: 2;
-  }
-  :global(#right-bar) {
-    position: absolute;
-    top: 0%;
-    right: 0%;
-    width: 90vw;
-    height: 90vh;
-
-    margin: 2% 5%;
-    padding: 2em;
-    text-align: left;
-    color: black;
-    border-radius: 10px;
-    background-color: antiquewhite;
-  }
-</style>
---
+++ src/components/routes/Main.svelte
@@ -0,0 +1,181 @@
+<script>
+  import Map, { map } from "../Map.svelte";
+  import ResearchMap from "../ResearchMap.svelte";
+  import Filter from "../Filter.svelte";
+  import FileUploadModal from "../FileUploadModal.svelte";
+  import FileNavigator from "../FileNavigator.svelte";
+
+  import SearchSemanticScholar from "../SearchSemanticScholar.svelte";
+  import {
+    Drawer,
+    Button,
+    CloseButton,
+    Accordion,
+    AccordionItem,
+  } from "flowbite-svelte";
+  import { OpenBookSolid } from "flowbite-svelte-icons";
+  import { sineIn } from "svelte/easing";
+
+  import { tick } from "svelte";
+
+  export let supabase;
+  let hiddenResearchMap = true;
+  let transitionParams = {
+    x: -320,
+    duration: 700,
+    easing: sineIn,
+  };
+</script>
+
+<main>
+  <div id="mapContainer">
+    <Map />
+
+    <div id="left-bar" class="p-4">
+      <div id="meta-info" class="">
+        <h1 class="font-bold text-4xl">Narrative Maker</h1>
+        <h3>
+          This is a prototype to explore linked stories of geospatial data
+        </h3>
+      </div>
+
+      <Accordion flush>
+        <aside id="data-ops">
+          <AccordionItem
+            class="bg-primary-50 !py-0"
+            paddingFlush="py-1"
+            padding="p-0"
+          >
+            <h3 slot="header">Filter</h3>
+            <section id="data-ops-inner">
+              <div id="filter" class="m-3 px-3">
+                <Filter />
+              </div>
+            </section>
+          </AccordionItem>
+        </aside>
+
+        <aside id="sources">
+          <AccordionItem class="bg-primary-50" paddingFlush="py-1" open>
+            <h3 slot="header">Sources</h3>
+            <section id="sources-inner">
+              <div id="parseFile" class="m-2 px-3">
+                <FileNavigator />
+              </div>
+
+              <div id="searchSemanticScholar" class="m-2 px-3">
+                <SearchSemanticScholar />
+              </div>
+            </section>
+          </AccordionItem>
+        </aside>
+      </Accordion>
+    </div>
+  </div>
+
+  <span id="right-bar-trigger" class="text-center">
+    <Button
+      color="dark"
+      class="z-10 absolute bg-primary-50 text-black hover:text-white top-5 right-5 w-50"
+      on:click={() => (hiddenResearchMap = false)}
+    >
+      <OpenBookSolid color="light" svgClass="mr-3" />Open research map</Button
+    >
+  </span>
+  <Drawer
+    placement="right"
+    transitionType="fly"
+    {transitionParams}
+    bind:hidden={hiddenResearchMap}
+    id="right-bar"
+  >
+    <CloseButton on:click={() => (hiddenResearchMap = true)} id="closeButton" />
+    <section id="markup" class="">
+      <h2 class="text-2xl">Research map</h2>
+      <p class="">
+        Imagine this to be a mindmap. Draw connections between documents as you
+        go along
+      </p>
+    </section>
+
+    <ResearchMap {supabase} />
+  </Drawer>
+  <FileUploadModal />
+</main>
+
+<style global>
+  main {
+    max-height: 100vh;
+    overflow: hidden;
+  }
+  :global(#closeButton) {
+    position: absolute;
+    top: 2%;
+    right: 2%;
+    background-color: #475569;
+  }
+  #markup {
+    margin-bottom: 2em;
+    padding: 0em;
+    font-size: larger;
+  }
+  main {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+  }
+  #mapContainer {
+    width: 100vw;
+    height: 100vh;
+  }
+  #left-bar {
+    position: absolute;
+    top: 2%;
+    left: 2%;
+    max-height: 100vh;
+    text-align: left;
+    width: 20%;
+    color: black;
+  }
+
+  #sources,
+  #data-ops,
+  #meta-info {
+    border-radius: 10px;
+    width: 100%;
+    margin-top: 1em;
+    color: black;
+    background-color: #faebd7;
+  }
+
+  #meta-info {
+    padding: 1em;
+  }
+  :global(.card) {
+    padding: 0.2em;
+  }
+  :global(.card-header) {
+    padding: 0em;
+    color: black !important;
+  }
+
+  #right-bar-trigger {
+    position: absolute;
+    top: 2%;
+    right: 2%;
+    z-index: 2;
+  }
+  :global(#right-bar) {
+    position: absolute;
+    top: 0%;
+    right: 0%;
+    width: 90vw;
+    height: 90vh;
+
+    margin: 2% 5%;
+    padding: 2em;
+    text-align: left;
+    color: black;
+    border-radius: 10px;
+    background-color: antiquewhite;
+  }
+</style>

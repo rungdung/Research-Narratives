34029ac32b96d688cc46918d1e7e86bb3bcfae1a
Adhavan <asivaraj@iihs.ac.in>
2023-07-30 15:01:52 +0530

    Generify Alert component

--- src/components/Alerts.svelte
+++ src/components/Alerts.svelte
@@ -1,55 +1,40 @@
 <script>
   import { uploadedSources, markupNodes } from "../stores";
   import { Toast, Button } from "flowbite-svelte";
+  import { onMount } from "svelte";
 
   let show = true;
   let counter = 7;
-  function trigger() {
-    show = true;
-    counter = 7;
-    timeout();
-  }
+  export let pos;
+  export let content;
 
   function timeout() {
     if (--counter > 0) return setTimeout(timeout, 1000);
     show = false;
   }
 
-  $: if ($uploadedSources.length > 0) {
-    trigger();
-  }
-
-  $: if ($markupNodes.length > 0) {
-    trigger();
-  }
+  // trigger timer on mount
+  onMount(() => {
+    timeout();
+  });
 </script>
 
-{#key $uploadedSources}
-  {#if $uploadedSources.length > 0}
-    <Toast
-      bind:open={show}
-      color="green"
-      class="absolute bottom-3 left-0 right-0 mx-auto w-60 text-black"
-    >
-      <span
-        ><b>{$uploadedSources[$uploadedSources.length - 1].fileName}</b></span
-      >
-      <span class="font-medium">has been uploaded :D</span>
-    </Toast>
-  {/if}
-{/key}
+{#if pos == "bottom"}
+  <Toast
+    bind:open={show}
+    color="green"
+    class="absolute bottom-3 left-0 right-0 mx-auto w-60 text-black"
+  >
+    {content}
+  </Toast>
+{/if}
 
-{#key $markupNodes.length}
-  {#if $markupNodes.length > 0}
-    <Toast
-      bind:open={show}
-      color="green"
-      class="absolute z-10 right-10 top-40 mx-auto w-50  text-black"
-    >
-      <span class="font-medium"
-        >Feature from <b>{$markupNodes[$markupNodes.length - 1].source}</b> is now
-        available for annotation</span
-      >
-    </Toast>
-  {/if}
-{/key}
+{#if pos == "right"}
+  <Toast
+    bind:open={show}
+    color="green"
+    class="absolute z-10 right-10 top-40 mx-auto w-50  text-black"
+  >
+    {content}
+  </Toast>
+{/if}

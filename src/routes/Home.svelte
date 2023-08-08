4fb44450bbcc82ed9ff064743a4e546063d51136
Adhavan Sivaraj <adhavan.sivaraj16@gmail.com>
2023-08-08 10:48:52 +0530

    Home Page with media

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -9,125 +9,167 @@
     DropdownItem,
     Chevron,
     DropdownDivider,
+    Card,
+    Button,
   } from "flowbite-svelte";
 
-  import Scrolly from "../components/Scrolly.svelte";
+  import { fly } from "svelte/transition";
+  let sequence = [
+    {
+      id: "spatial-whiteboarding",
+      text: "What if spatial data and media could be explored on a whiteboard?",
+      visual: "../../assets/researchMap.webm",
+    },
+    {
+      id: "filtering",
+      text: "Visualise your spatial data with filters and Choropleths",
+      visual: "../../assets/spatialFeatures.webm",
+    },
+    {
+      id: "rendered-stories",
+      text: "Convert your mindmaps into interactive data stories",
+      visual: "../../assets/renderedStory.webm",
+    },
+    {
+      id: "collaboration",
+      text: "When you share your story, your mindmap is shared alongside, contextualise to a new level",
+    },
+  ];
 
-  let value = 0,
-    sequence = [
-      {
-        text: "What if research and data analysis was a more visual experience that could be communicated?",
-        visual: "../../assets/home-example-1.png",
-      },
-      {
-        text: "Upload your resources and annotate all of them in the same space",
-        visual: "https://i.imgur.com/8Z1Q1ZM.png",
-      },
-      {
-        text: "Create a mindmap of annotations between resources",
-        visual: "https://i.imgur.com/8Z1Q1ZM.png",
-      },
-      {
-        text: "Make interactive data stories to share",
-        visual: "https://i.imgur.com/8Z1Q1ZM.png",
-      },
-      {
-        text: "If you want to share your annotations, share them along with your data",
-        visual: "https://i.imgur.com/8Z1Q1ZM.png",
-      },
-    ];
+  let sampleStories = [
+    {
+      title: "Bengaluru's Land-Use Change Trends",
+      description:
+        "..., the data from 2007-2019 showcases how 'Hi-Tech' a land use introduced specially in the 2007 master plan to promote IT industries has become the new location for residential and commercial real estate in the city. ",
+      image: "../../assets/landuse-bengaluru-sample-thumbnail.jpg",
+      link: "./demo/RenderedStory?uuid=4be2d2ce-8431-48d4-8ae7-c1a56177e98c",
+    },
+    {
+      title: "Geographical Patterns of Child Malnutrition in Karnataka",
+      description:
+        "Even after making decades of progress, India is still home to a major fraction of the world's malnourished children. Malnutrition in early childhood is associated with lower cognitive and physical ability, and can have a lifelong impact...",
+      image: "../../assets/stunting-karnataka-sample-thumbnail.jpg",
+      link: "./demo/RenderedStory?uuid=4e0aeb83-702e-4403-868a-ff081622229d",
+    },
+    {
+      title: "Try out the proof of concept",
+      description:
+        "You can upload your own data and create your own stories. The tool is in its alpha stages, so please bear with us. We are looking for contributors to help us build this tool. If you are interested, please reach out to us at...",
+      image: "../../assets/empty-home-sample-thumbnail.jpg",
+      link: "./demo/",
+      type: "demo",
+    },
+  ];
+
+  let roadMap;
 </script>
 
-<Navbar let:hidden let:toggle class="!bg-primary w-screen mx-5 my-5">
-  <NavBrand href="/">
-    <h1
-      class="self-center whitespace-nowrap text-4xl font-semibold text-yellow-500 dark:text-white"
-    >
-      Research Narratives
-    </h1>
-  </NavBrand>
-  <NavHamburger on:click={toggle} />
-  <NavUl {hidden}>
-    <NavLi href="/" active={true}>Home</NavLi>
-    <NavLi id="nav-menu1" class="cursor-pointer"
-      ><Chevron aligned>Possibilities</Chevron></NavLi
-    >
-    <NavLi href="">Going forward</NavLi>
-    <NavLi href="">Alpha</NavLi>
-    <NavLi href="">Contact</NavLi>
-    <Dropdown triggeredBy="#nav-menu1" class="w-44 z-20">
-      <DropdownItem>Spatial Data exploration</DropdownItem>
-      <DropdownItem>Mindmapping data</DropdownItem>
-      <DropdownItem>Share and collaborate</DropdownItem>
-    </Dropdown>
-  </NavUl>
-</Navbar>
+<section class="bg-primary-50 text-black">
+  <Navbar
+    let:hidden
+    let:toggle
+    class="bg-gradient-to-b from-primary-100 to-primary-50   py-5"
+    navDivClass="items-center px-6 py-10 "
+  >
+    <NavBrand href="/">
+      <h1
+        class="text-5xl w-1/3 text-center md:mx-0 md:text-left font-semibold text-yellow-500"
+      >
+        Research Narratives
+      </h1>
+      <br />
+    </NavBrand>
+    <p class="text-left md:w-1/3 w-full py-3 text-yellow-500">
+      A visual interface for multi-media and spatial data annotation. In alpha,
+      looking for contributors
+    </p>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden} activeClass="" divClass="w-1/2 md:block ">
+      <NavLi id="nav-menu1" class="cursor-pointer"
+        ><Chevron aligned>Possibilities</Chevron></NavLi
+      >
+      <Dropdown triggeredBy="#nav-menu1" class="w-44 z-20">
+        <DropdownItem href="/#filtering">Spatial Data exploration</DropdownItem>
+        <DropdownItem href="/#spatial-whiteboarding"
+          >Mindmapping data</DropdownItem
+        >
+        <DropdownItem href="/#collaboration">Share and collaborate</DropdownItem
+        >
+        <DropdownItem href="/#rendered-stories">Create datastories</DropdownItem
+        >
+      </Dropdown>
+      <NavLi href="/#roadmap">Going forward</NavLi>
+      <NavLi href="/#demo">Alpha</NavLi>
+      <NavLi href="/#github">Contact</NavLi>
+    </NavUl>
+  </Navbar>
 
-<body class="section-container h-screen">
-  <section id="scrolly" class="mx-auto h-screen">
-    <section class="steps-container">
-      <Scrolly bind:value>
-        {#each sequence as text, i}
-          <div class="step" class:active={value == i}>
-            <section id="text" class="p-6 flex text-left m-3 items-center">
-              <h2 class="text-5xl block">
-                {@html text.text}
-              </h2>
-            </section>
-            <section
-              id="visual-content"
-              class="shadow-2xl object-contain shadow-black"
-            >
-              {#if value >= 0}
-                <img
-                  src={sequence[value].visual}
-                  alt="preview"
-                  class="h-screen object-cover"
-                />
-              {/if}
-            </section>
-          </div>
-        {/each}
-      </Scrolly>
+  <section class="grid md:grid-cols-2 sm:grid-cols-2">
+    {#each sequence as { id, text, visual }}
+      <section id="text" class="p-6 text-left m-3 max-w-prose sm:pt-10 sm:h-50">
+        <h2 {id} class="sm:text-4xl text-2xl align-middle">
+          {@html text}
+        </h2>
+      </section>
+
+      {#if visual}
+        <section
+          id="visual-content"
+          class="shadow-2xl object-contain shadow-black mx-5 sm:mx-1 sm:my-10"
+          in:fly={{ x: 100, duration: 800 }}
+        >
+          <video class=" md:object-cover" autoplay muted loop>
+            <source src={visual} type="video/webm" />
+          </video>
+        </section>
+      {/if}
+    {/each}
+  </section>
+  <section id="trial" class="w-screen p-5 text-left content-center mx-auto">
+    <h2 id="demo" class="text-4xl text-center py-10">
+      Check these sample stories out
+    </h2>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      {#each sampleStories as { title, description, image, link, type }}
+        <Card class="p-0 bg-brown drop-shadow m-3" img={image}>
+          <h2 class="text-2xl text-black py-2">{title}</h2>
+          <p>{description}</p>
+          <Button class="bg-gray-500 my-4" size="xs" href={link}>View</Button>
+        </Card>
+      {/each}
     </section>
   </section>
-</body>
-<section id="trial" class="w-screen">
-  Check out these sample data stories
+
+  <section id="roadmap">
+    The project is being tracked on a public Github Project here: <a
+      href="https://github.com/users/rungdung/projects/3/views/1"
+      target="_blank"
+    >
+      Link
+    </a>
+  </section>
 </section>
-<footer class="w-screen">
-  <h1>Thanks!</h1>
-  <h2>
+
+<footer
+  class="w-screen bg-gradient-to-t from-primary-200 to-primary-50 text-black text-left px-10 py-40 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 space-x-5"
+>
+  <p class="pr-5">
+    This project is a proof of concept in development and requires active
+    contributors. If you share our vision or can contribute time and effort to
+    develop this, reach out to us on Github Issues. This proof of concept was
+    developed as part of an internship at the Indian Institute for Human
+    Settlements, Bengaluru.
+  </p>
+
+  <p>This project is being developed under the Apache license</p>
+  <p id="links">
+    Email at adhavan16[dot]sivaraj[at]gmail[dot]com
     <a href="" target="_blank">Github</a>
-  </h2>
+  </p>
 </footer>
 
 <style>
-  :global(body) {
-    overflow-x: hidden;
-  }
-  .step {
-    display: flex;
-    flex-direction: row;
-    height: 100vh;
-    width: 100%;
-  }
-  #text {
-    flex: 40%;
-  }
-
-  #visual-content {
-    flex: 60%;
-  }
-
-  /* Comment out the following line to always make it 'text-on-top' */
-  @media screen and (max-width: 768px) {
-    .section-container {
-      flex-direction: column-reverse;
-    }
-    .sticky {
-      width: 95%;
-      margin: auto;
-    }
+  :global(html) {
+    scroll-behavior: smooth;
   }
 </style>

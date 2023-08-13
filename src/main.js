3df904d8e9a6594c5dacc183a39e074e20779a56
Adhavan Sivaraj <adhavan.sivaraj16@gmail.com>
2023-08-13 12:29:11 +0530

    Posthog analytics initial test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import App from "./App.svelte";
 import "./app.css";
 import { createClient } from "@supabase/supabase-js";
+import posthog from "posthog-js";
+
+// PostHog for analytics
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: "https://app.posthog.com",
+});
 
 // Create a Supabase client instance
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;

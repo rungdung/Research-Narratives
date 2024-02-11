4f1b690f1786a33d59942a80369ba0085c581888
Adhavan <adhavan.sivaraj16@gmail.com>
2024-02-11 22:29:42 +0530

    feature, optimisations, cleanup: - Refactored to let data be loaded onMount, instead of reloading at every appearance - Spatial data now smoothly flies in sequence - Map is loaded and kept hidden to stop reloading at every appearance - All spatial data is loaded on the same map, so that flyTo operations are possible

--- src/lib/research-map/spatial-annotate/SpatialView.svelte
+++ src/lib/research-map/spatial-annotate/SpatialView.svelte
@@ -1,21 +1,33 @@
 <script>
+	/**
+	 * Initialize the map and handle resource fetching and display based on spatialEnabled and resource properties.
+	 * @param {Object} resource - The resource object containing details like id, filterExpression, center, zoom, pitch, bearing.
+	 * @param {Blob} resourceBlob - The blob containing the resource data.
+	 * @param {boolean} spatialEnabled - Flag indicating if spatial is enabled.
+	 */
 	import maplibre from 'maplibre-gl';
 	import 'maplibre-gl/dist/maplibre-gl.css';
 	import { onMount } from 'svelte';
-	import { supabase } from '$lib/supabaseClient';
 
 	import { PUBLIC_MAPTILER_KEY } from '$env/static/public';
 
 	export let resource;
+	export let resourceBlob;
+	export let spatialEnabled;
 
 	let map, filterExpression, appearanceExpression, mapContainer, resourceJSON;
-	filterExpression = resource.filterExpression;
+	filterExpression = resource?.filterExpression || '';
+
+	let addedSources = [];
 
 	// Color settings
 	const colorsOG = ['#6E07EB', '#00CAF5', '#5FDE43', '#F5C83D', '#EB533B'];
 	let colors = colorsOG.map((color) => color);
 
-	// Function to get a random color
+	/**
+	 * Get a random color from the colors array and remove it from the array.
+	 * @returns {string} - Random color from the colors array.
+	 */
 	function getRandColor() {
 		let selectedColor;
 		let randIndex = Math.floor(Math.random() * colors.length);
@@ -30,106 +42,109 @@
 		return selectedColor;
 	}
 
-	// Initialization on mount
-	onMount(() => {
-		// Calculate the initial center, zoom, and pitch by subtracting from the final values
-		const initialCenter = {lat:resource.center.lat - 0.005, lng: resource.center.lng - 0.005};
-		const initialZoom = resource.zoom - 6;
-		const initialPitch = resource.pitch - 5;
+	/**
+	 * Fetch the resource data from the provided blob and parse it.
+	 * @param {Blob} resourceBlob - The blob containing the resource data.
+	 */
+	const resourceFetch = async (resourceBlob) => {
+		await fetch(resourceBlob).then(async (response) => {
+			resourceJSON = await response.json();
+		});
+	};
 
-		console.log(resource, initialZoom, initialPitch);
-		// Map initialization
+	const addLayer = async () => {
+		await resourceFetch(resourceBlob);
+		// Handling map load event
+		if (!addedSources.includes(resource.id) && !map.getLayer(resource.id)) {
+			// Download and parse the resource data
+			let geometryType = resourceJSON.features[0].geometry.type,
+				type;
+			let filter, appearance;
 
+			// Add source and layer to the map
+			map.addSource(resource.id, {
+				type: 'geojson',
+				data: resourceJSON
+			});
+
+			// Set filter and appearance based on geometry type
+			if (['Polygon', 'MultiPolygon'].includes(geometryType)) {
+				filter = ['==', '$type', 'Polygon'];
+				appearance = {
+					'fill-color': getRandColor(),
+					'fill-opacity': 0.6,
+					'fill-outline-color': 'green'
+				};
+				type = 'fill';
+			} else if (['LineString', 'MultiLineString'].includes(geometryType)) {
+				filter = ['==', '$type', 'LineString'];
+				appearance = {
+					'line-color': getRandColor()
+				};
+				type = 'line';
+			} else if (['Point', 'MultiPoint'].includes(geometryType)) {
+				filter = ['==', '$type', 'Point'];
+				appearance = {
+					'fill-color': getRandColor(),
+					'fill-opacity': 0.6,
+					'fill-outline-color': 'green'
+				};
+				type = 'fill';
+			}
+
+			map.addLayer({
+				id: resource.id + 'resource-layer',
+				type: type,
+				source: resource.id,
+				filter: filter,
+				paint: appearance
+			});
+
+			// If filter exists already, apply it
+			map.setFilter(resource.id + 'resource-layer', resource.filterExpression);
+
+			addedSources.push(resource.id);
+		}
+
+		if (resource?.center)
+			map.flyTo({
+				center: resource.center,
+				zoom: resource.zoom,
+				pitch: resource.pitch,
+				bearing: resource.bearing
+			});
+	};
+
+	/**
+	 * Event handler for map initialization and resource loading.
+	 */
+	onMount(async () => {
 		// Map initialization
 		map = new maplibre.Map({
 			container: mapContainer,
 			style: `https://api.maptiler.com/maps/47780736-e784-40ca-9f2e-6da4248ada51/style.json?key=${PUBLIC_MAPTILER_KEY}`,
-			center: initialCenter || [0, 0],
-			pitch: initialPitch || 0,
-			bearing: resource.bearing || 0,
-			zoom: initialZoom || 3,
+			center: [0, 0],
+			pitch: 0,
+			bearing: 0,
+			zoom: 3,
 			maxZoom: 14,
-			minZoom: 3
+			minZoom: 3,
+			transformRequest: (url) => {
+				// cache header
+				return {
+					url: url,
+					cache: 'force-cache'
+				};
+			}
 		});
-
-		// Handling map load event
-		try {
-			map.on('load', async () => {
-				// Download and parse the resource data
-				let blob = await downloadResource(resource.url);
-				resourceJSON = JSON.parse(await blob.text());
-				let geometryType = resourceJSON.features[0].geometry.type,
-					type;
-				let filter, appearance;
-
-				// Add source and layer to the map
-				map.addSource('resource', {
-					type: 'geojson',
-					data: resourceJSON
-				});
-
-				// Set filter and appearance based on geometry type
-				if (['Polygon', 'MultiPolygon'].includes(geometryType)) {
-					filter = ['==', '$type', 'Polygon'];
-					appearance = {
-						'fill-color': getRandColor(),
-						'fill-opacity': 0.6,
-						'fill-outline-color': 'green'
-					};
-					type = 'fill';
-				} else if (['LineString', 'MultiLineString'].includes(geometryType)) {
-					filter = ['==', '$type', 'LineString'];
-					appearance = {
-						'line-color': getRandColor()
-					};
-					type = 'line';
-				} else if (['Point', 'MultiPoint'].includes(geometryType)) {
-					filter = ['==', '$type', 'Point'];
-					appearance = {
-						'fill-color': getRandColor(),
-						'fill-opacity': 0.6,
-						'fill-outline-color': 'green'
-					};
-					type = 'fill';
-				}
-
-				map.addLayer({
-					id: 'resource-layer',
-					type: type,
-					source: 'resource',
-					filter: filter,
-					paint: appearance
-				});
-
-				// If filter exists already, apply it
-				map.setFilter('resource-layer', resource.filterExpression);
-
-				map.flyTo({
-					center: resource.center,
-					zoom: resource.zoom,
-					pitch: resource.pitch,
-					bearing: resource.bearing
-				});
-			});
-		} catch (e) {
-			console.log(e);
-		}
 	});
 
-	// Function to download resource from Supabase storage
-	const downloadResource = async (path) => {
-		try {
-			const { data, error } = await supabase.storage.from('resources').download(path);
-			if (error) {
-				throw error;
-			}
-			return data;
-		} catch (error) {
-			if (error instanceof Error) {
-				console.log('Error downloading resource: ', error.message);
-			}
-		}
-	};
+	/**
+	 * Fetch the resource data if the resourceBlob is provided.
+	 */
+	$: if (resourceBlob && spatialEnabled) {
+		addLayer();
+	}
 </script>
 
 <!-- Map and sidebar section -->
--- src/routes/libraries/narratives/view/+page.svelte
+++ src/routes/libraries/narratives/view/+page.svelte
@@ -1,31 +1,36 @@
 <script>
-	import { supabase } from '$lib/supabaseClient';
 	import Scroller from '@sveltejs/svelte-scroller';
 
 	import { fly, blur } from 'svelte/transition';
 	import SpatialView from '$lib/research-map/spatial-annotate/SpatialView.svelte';
+	import { onMount } from 'svelte';
 
 	export let data;
-	let preview;
+	let supabase = data.supabase;
+	$: supabase = data.supabase;
 
-	let index = 1; // int index of currently active foreground DOM element
+	let index = 0; // int index of currently active foreground DOM element
 	let offset; // float offset of currently active foreground DOM element - 0 to 1 value
 	let progress; // float how far along the whole scrolly we currently are - 0 to 1 value
 	let count; // int total num steps (=DOM elements) of the foreground
 
 	let top = 0; // The vertical position that the top of the foreground must scroll past before the background becomes fixed, as a proportion of window height
 	let bottom = 1; // Once the bottom of the foreground passes this point, the background becomes unfixed
-	let threshold = 0.5; // Once a foreground step is this much visible in viewport, it becomes 'active'
+	let threshold = 0.4; // Once a foreground step is this much visible in viewport, it becomes 'active'
+
+	let spatialPreview,
+		spatialEnabled = false,
+		mediaEnabled = false,
+		preview,
+		loaded;
 
 	// Get preview
 	const downloadResource = async (path) => {
 		try {
 			const { data, error } = await supabase.storage.from('resources').download(path);
-
 			if (error) {
 				throw error;
 			}
-
 			return URL.createObjectURL(data);
 		} catch (error) {
 			if (error instanceof Error) {
@@ -33,52 +38,88 @@
 			}
 		}
 	};
+
+	const loadData = async (index) => {
+		// If spatial data
+		// enable display and set spatialPreview
+		if (data.narrative.narrative_sections[index].displayObj.type == 'geojson') {
+			spatialPreview = data.narrative.narrative_sections[index].preview;
+			spatialEnabled = true;
+			mediaEnabled = false;
+		} else if (
+			['jpg', 'png', 'jpeg'].includes(data.narrative.narrative_sections[index].displayObj.type)
+		) {
+			preview = data.narrative.narrative_sections[index].preview;
+			spatialEnabled = false;
+			mediaEnabled = true;
+		} else {
+			spatialEnabled = false;
+			mediaEnabled = false;
+		}
+	};
+
+	const extractPreviews = async () => {
+		await Promise.all(
+			data.narrative.narrative_sections.map(async (section, index) => {
+				if (section.displayObj.url) {
+					section.preview = await downloadResource(section.displayObj.url);
+				}
+			})
+		);
+	};
+
+	onMount(async () => {
+		await extractPreviews();
+	});
+
+	$: if (data) {
+		loadData(index);
+	}
 </script>
 
-<div class="text-black h-screen overflow-y-auto grid place-items-center py-3">
+<div class="text-black h-screen overflow-y-auto grid place-items-center mb-10">
 	<div class="w-3/4">
 		<h1>{data.narrative.title}</h1>
 		{@html data.narrative.description}
 	</div>
 </div>
-<Scroller {top} {bottom} {threshold} bind:index bind:offset bind:progress bind:count>
-	<div slot="background">
-		{#if data.narrative.narrative_sections[index]}
-			{#if ['jpg', 'png', 'jpeg'].includes(data.narrative.narrative_sections[index].displayObj.type)}
-				{#await (preview = downloadResource(data.narrative.narrative_sections[index].displayObj.url))}
-					Loading...
-				{:then preview}
-					<div transition:blur={{ duration: 200 }}>
-						<img src={preview} alt="preview" class="h-screen object-contain ml-auto" />
 
-						{#if data.narrative.narrative_sections[index].annotation}
-							<p class="text-grey-200">
-								{data.narrative.narrative_sections[index].annotation}
-							</p>
-						{/if}
-					</div>
-				{/await}
-			{:else if ['geojson'].includes(data.narrative.narrative_sections[index].displayObj.type)}
-				<section class="h-[100vh] w-[100vw]">
-					<SpatialView bind:resource={data.narrative.narrative_sections[index].displayObj} />
-				</section>
+	<Scroller {top} {bottom} {threshold} bind:index bind:offset bind:progress bind:count>
+		<div slot="background">
+			{#if mediaEnabled}
+				<div transition:blur={{ duration: 200 }}>
+					<img src={preview} alt="preview" class="h-screen object-contain ml-auto" />
+					{#if data.narrative.narrative_sections[index].annotation}
+						<p class="text-grey-200">
+							{data.narrative.narrative_sections[index].annotation}
+						</p>
+					{/if}
+				</div>
 			{/if}
-		{/if}
-	</div>
-	<div class="snap-y snap-mandatory" slot="foreground">
-		{#each data.narrative.narrative_sections as section}
-			<div
-				class="snap-top bg-opacity-70 ml-10 py-10 min-h-screen w-1/3 text-black bg-primary-50 rounded-lg"
+			<section
+				transition:blur={{ duration: 200 }}
+				class="h-[100vh] w-[100vw] {spatialEnabled ? '' : 'hidden'}"
 			>
-				<section class="p-3 my-auto">
-					<h2 class="text-3xl">{section.title}</h2>
-					{@html section.bodytext}
-				</section>
-			</div>
-		{/each}
-	</div>
-</Scroller>
-
+				<SpatialView
+					bind:resource={data.narrative.narrative_sections[index].displayObj}
+					bind:resourceBlob={data.narrative.narrative_sections[index].preview}
+					bind:spatialEnabled
+				/>
+			</section>
+		</div>
+		<div class="snap-y snap-mandatory" slot="foreground">
+			{#each data.narrative.narrative_sections as section}
+				<div
+					class="snap-center bg-opacity-70 ml-10 py-10 min-h-screen w-1/3 text-black bg-primary-50 rounded-lg"
+				>
+					<section class="p-3 my-auto">
+						<h2 class="text-3xl">{section.title}</h2>
+						{@html section.bodytext}
+					</section>
+				</div>
+			{/each}
+		</div>
+	</Scroller>
 <footer
 	class="bg-transparent text-yellow-900 text-left py-40 px-2 sm:px-10 md:w-3/4 md:mx-auto grid grid-cols-1 sm:grid-cols-2"
 >
@@ -110,4 +151,4 @@
 
 <svelte:head>
 	<title>Story: {data.narrative.title}</title>
-</svelte:head> 
+</svelte:head>
